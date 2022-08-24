42f707fc9015db310112e555210545f65a7486a0
gaowenbin <lotosv2010@163.com>
2022-08-24 11:34:15 +0800

    feat(init): 初始化项目

---
+++ components/Tutorial.vue
@@ -0,0 +1,24 @@
+<!-- Please remove this file from your project -->
+<template>
+  <div class="nuxt-tutorial">
+    <Logo />
+    <p>hello nuxt</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NuxtTutorial'
+}
+</script>
+
+<style scoped lang="scss">
+// https://juejin.cn/post/7039508283190673416
+.nuxt-tutorial {
+  text-align: center;
+  .nuxt-logo {
+    height: 80px;
+  }
+}
+
+</style>
